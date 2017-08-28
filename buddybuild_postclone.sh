263eefeebb423b3c162a73854cd155556681c797
jeremypele <jeremy.pele@gmail.com>
2017-08-28 14:20:30 -0400

    Update conf

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -8,7 +8,9 @@ npm install -g nativescript
 echo "******** NativeScript CLI Ready *************"
 tns --version
 
+# Display last commii
+echo $(git log -1 --pretty=format:"%an <%ae>")
+
 # Prepare NativeScript Project
 echo "******** Prepare NativeScript Android Project *************"
-echo "tns prepare android"
-tns build android
+tns prepare android
